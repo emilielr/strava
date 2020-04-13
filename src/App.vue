08ac0bd192fe7ac798fe33a21026eb7a4d63ced7
Erling <hei@erli.ng>
2020-04-13 15:32:01 +0200

    abakus and omega

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623241/latest-rides/aa1a47045f65f38d41da8fb35e23d8373357ed43?show_rides=false' width='300'></iframe>
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/622520/latest-rides/a026c406d22eda97af1ca7cbe30088c44c038e3a?show_rides=false' width='300'></iframe>
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/232813/latest-rides/e6d07cf4484d12cc95df22b3f102db968bcf871b?show_rides=false' width='300'></iframe>
   </div>
 </template>
 
@@ -20,5 +22,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
+
+iframe:not(:last-child) {
+  margin-bottom: 20px;
+}
+
 </style>
