bda0a6509a6d7c71c3a7abed333e152f6c50b6c7
emilielr <43408175+emilielr@users.noreply.github.com>
2020-11-08 17:54:26 +0100

    tohub

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
   <div id="app">
-  <!-- Abakus, Omega, Spanskrøret, EMIL, HC, Timini, Nabla, Broderskapets lange løpere, Tihlde, MiT, Elektra, Delta, Vivas, Volvox, KoM, Nucleus -->
+  <!-- Abakus, Omega, EMIL, Broderskapets lange løpere, Bekk -->
   <!-- 
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/622520/latest-rides/a026c406d22eda97af1ca7cbe30088c44c038e3a?show_rides=false' width='300'></iframe>
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/232813/latest-rides/e6d07cf4484d12cc95df22b3f102db968bcf871b?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623691/latest-rides/f6fbaef2c1fb8b2290b0b30f9bd0e5349d178812?show_rides=false' width='300'></iframe>
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/622664/latest-rides/c217105223a08ff63c92409a7a8275465b373eb0?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623310/latest-rides/5c9c40a114b3db1cf3370e2106ff2c73b1f9a1c3?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623368/latest-rides/4787a207557a9963a3e5ad439b448ba17d87b9f0?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623368/latest-rides/4787a207557a9963a3e5ad439b448ba17d87b9f0?show_rides=false' width='300'></iframe>
+    
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/108144/latest-rides/9ca775291d850331f3852406014c2ac77fea76e7?show_rides=false' width='300'></iframe>
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/546647/latest-rides/e74a3aa427856e7b570e094b6a6f39a38c332b2f?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/627287/latest-rides/e6bee1a84ab19c5487c7df657aaa02cff0fc0c45?show_rides=false' width='300'></iframe>
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623691/latest-rides/f6fbaef2c1fb8b2290b0b30f9bd0e5349d178812?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/630739/latest-rides/e61b0691a1366e839bb13470776bff226581b675?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/631214/latest-rides/ee54c420ded23c237454c9b41ebdd675193c9393?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/640213/latest-rides/3c87d8fb1704418652c9e7f30729101221531a46?show_rides=false' width='300'></iframe>
+    <iframe id="bekk" allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/8433/latest-rides/a6513fe72d71c5979c5247767a350b43cfa476ce?show_rides=false' width='300'></iframe>
+  -->
+
+  <!-- Online, MiT, Vivas, Delta, Volvox, KoM, Nucleus, Timini, Nabla, Elektra, Thilde, Spanskrøret, HC -->    
+    <iframe id="online" allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623241/latest-rides/aa1a47045f65f38d41da8fb35e23d8373357ed43?show_rides=false' width='300'></iframe>
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/627287/latest-rides/e6bee1a84ab19c5487c7df657aaa02cff0fc0c45?show_rides=false' width='300'></iframe>
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/637480/latest-rides/3913b088b4080fdaf5e2eb1bd7cba38a2a227cf5?show_rides=false' width='300'></iframe>
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/640213/latest-rides/3c87d8fb1704418652c9e7f30729101221531a46?show_rides=false' width='300'></iframe>
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/637774/latest-rides/19beb64fc981c4859294f267c6eb15b114d151a7?show_rides=false' width='300'></iframe>
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/635644/latest-rides/d2f3735d661d09aba4be38e8382214e964937c07?show_rides=false' width='300'></iframe>
     <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/637052/latest-rides/d9068e4a97c62ded99301b6bf8ee74f4b33d35cf?show_rides=false' width='300'></iframe> 
-  -->
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623368/latest-rides/4787a207557a9963a3e5ad439b448ba17d87b9f0?show_rides=false' width='300'></iframe>
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623368/latest-rides/4787a207557a9963a3e5ad439b448ba17d87b9f0?show_rides=false' width='300'></iframe>  
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/631214/latest-rides/ee54c420ded23c237454c9b41ebdd675193c9393?show_rides=false' width='300'></iframe>
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/630739/latest-rides/e61b0691a1366e839bb13470776bff226581b675?show_rides=false' width='300'></iframe>
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623691/latest-rides/f6fbaef2c1fb8b2290b0b30f9bd0e5349d178812?show_rides=false' width='300'></iframe>
+    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623310/latest-rides/5c9c40a114b3db1cf3370e2106ff2c73b1f9a1c3?show_rides=false' width='300'></iframe>
 
-  <!-- Online, Bekk -->    
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/623241/latest-rides/aa1a47045f65f38d41da8fb35e23d8373357ed43?show_rides=false' width='300'></iframe>
-    <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/8433/latest-rides/a6513fe72d71c5979c5247767a350b43cfa476ce?show_rides=false' width='300'></iframe>
   </div>
 </template>
 
